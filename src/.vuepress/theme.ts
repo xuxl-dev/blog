9898dd7640796456d225890565f67ce28bf12c24
bkmashiro <dylan_233@foxmail.com>
2023-11-27 21:45:43 +0800

    fix repo setting

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -17,7 +17,7 @@ export default hopeTheme({
 
   logo: "http://via.placeholder.com/64x64",
 
-  repo: "vuepress-theme-hope/vuepress-theme-hope",
+  repo: "xuxl-dev/blog",
 
   docsDir: "src",
 
@@ -27,7 +27,7 @@ export default hopeTheme({
   // sidebar
   sidebar,
 
-  footer: "this is a footer",
+  footer: "this is a preview",
 
   displayFooter: true,
 
@@ -35,38 +35,10 @@ export default hopeTheme({
     description: "Prof @ NUIST",
     intro: "/intro.html",
     medias: {
-      Baidu: "https://example.com",
       BiliBili: "https://example.com",
-      Bitbucket: "https://example.com",
-      Dingding: "https://example.com",
-      Discord: "https://example.com",
-      Dribbble: "https://example.com",
       Email: "mailto:info@example.com",
-      Evernote: "https://example.com",
-      Facebook: "https://example.com",
-      Flipboard: "https://example.com",
-      Gitee: "https://example.com",
       GitHub: "https://example.com",
-      Gitlab: "https://example.com",
-      Gmail: "mailto:info@example.com",
-      Instagram: "https://example.com",
-      Lark: "https://example.com",
-      Lines: "https://example.com",
-      Linkedin: "https://example.com",
-      Pinterest: "https://example.com",
-      Pocket: "https://example.com",
-      QQ: "https://example.com",
-      Qzone: "https://example.com",
-      Reddit: "https://example.com",
       Rss: "https://example.com",
-      Steam: "https://example.com",
-      Twitter: "https://example.com",
-      Wechat: "https://example.com",
-      Weibo: "https://example.com",
-      Whatsapp: "https://example.com",
-      Youtube: "https://example.com",
-      Zhihu: "https://example.com",
-      MrHope: ["https://mister-hope.com", MR_HOPE_AVATAR],
     },
   },
 
