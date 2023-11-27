f1a703fbbc9c32ebee4eb8208a5112750cdc7cf3
bkmashiro <dylan_233@foxmail.com>
2023-11-27 22:20:23 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,7 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+import { registerComponentsPlugin } from "@vuepress/plugin-register-components";
+import path from "path";
 
 export default defineUserConfig({
   base: "/blog/",
@@ -9,7 +11,11 @@ export default defineUserConfig({
   description: "Xlxu Lab",
 
   theme,
-
+  plugins: [
+    registerComponentsPlugin({
+      componentsDir: path.resolve(__dirname, "./components"),
+    }) as any,
+  ],
   // Enable it with pwa
   // shouldPrefetch: false,
 });
